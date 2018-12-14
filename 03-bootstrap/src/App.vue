ee1801cfd24014b07a0cda7a4a11dd4e4163b060
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2018-12-14 16:10:02 +0100

    Add router

--- 03-bootstrap/src/App.vue
+++ 03-bootstrap/src/App.vue
@@ -1,40 +1,12 @@
 <template>
   <div id="app">
-    <b-container>
-
-      <b-row>
-        <b-col>
-          <div class="grey">A</div>
-        </b-col>
-        <b-col>
-          <div class="grey">B</div>
-        </b-col>
-        <b-col>
-          <div class="grey">C</div>
-        </b-col>
-      </b-row>
-
-      <b-row>
-        <b-col>
-          <b-alert show>ALERT</b-alert>
-        </b-col>
-      </b-row>
-
-    </b-container>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default Vue.extend({
-  name: "app",
-  components: {
-    HelloWorld
-  }
-});
-</script>
 
 <style lang="scss">
 @import './assets/scss/app.scss';
