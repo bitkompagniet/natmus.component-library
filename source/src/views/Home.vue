9b8de5c1c3b6cbe5b1e939ad2024e599d7c01979
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2018-12-21 10:55:47 +0100

    Remove HelloWorld

--- source/src/views/Home.vue
+++ source/src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
     <section-header>Wuut</section-header>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>This is a text.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 import SectionHeader from '@/components/exported/text/SectionHeader.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
     SectionHeader,
   },
 };
