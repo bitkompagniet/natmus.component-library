f7456bbbb482a03eef1221d1efe244318286fb70
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2019-01-24 18:30:11 +0100

    Footer draft

--- source/src/components/Footer.vue
+++ source/src/components/Footer.vue
@@ -11,10 +11,32 @@
 
 
 .standard {
+    font-family: 'Nationale';
+
     background-color: $color-darkblue;
     color: $color-white;
 
     padding: 4em 0;
+
+    h3 {
+        position: relative;
+        margin-bottom: 1em;
+
+        &::after {
+            content: "------";
+            color: transparent;
+            border-top: 2px solid $color-white;
+            position: absolute;
+            top: 1.5em;
+            left: 0;
+        }
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
 }
 
 </style>
--- source/src/demo/DemoApp.vue
+++ source/src/demo/DemoApp.vue
@@ -4,18 +4,37 @@
       <b-nav-item to="/">Structure</b-nav-item>
       <b-nav-item to="/components">Components</b-nav-item>
       <b-nav-item to="/forms">Forms</b-nav-item>
-      <b-nav-item to="/bootstrap-styling">Bootstrap styling</b-nav-item>
-      <b-nav-item to="/full">Full page example</b-nav-item>
+      <b-nav-item to="/bootstrap-styling">Styling</b-nav-item>
       <b-nav-item to="/text-page">Example: Text</b-nav-item>
       <b-nav-item to="/step-page">Example: Steps</b-nav-item>
+      <b-nav-item to="/full">Example: Mix</b-nav-item>
     </n-nav-topbar>
     <router-view />
     <n-footer>
       <b-row>
-        <b-col>BL</b-col>
-        <b-col>NM</b-col>
-        <b-col>Kontakt</b-col>
-        <b-col>Mere</b-col>
+        <b-col><img src="../assets/images/BL.svg" style="width: 120px" /></b-col>
+        <b-col><n-logo white /></b-col>
+        <b-col>
+          <h3>Generelt</h3>
+          <ul>
+            <li>Om os</li>
+            <li>Projektet</li>
+            <li>Vejledning</li>
+          </ul>
+        </b-col>
+        <b-col>
+          <h3>Følg os</h3>
+          <ul>
+            <li>Facebook</li>
+            <li>LinkedIn</li>
+            <li>Website</li>
+          </ul>
+        </b-col>
+      </b-row>
+      <b-row class="mt-3">
+        <b-col>
+          I samarbejde med BL - Danmarks Almene Boliger
+        </b-col>
       </b-row>
     </n-footer>
   </div>
