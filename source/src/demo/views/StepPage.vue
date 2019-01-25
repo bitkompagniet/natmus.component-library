7ec9377ada5323c8b99f0409891a1e61cf625494
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2019-01-25 12:44:53 +0100

    Correct introduction page texts

--- source/src/demo/views/StepPage.vue
+++ source/src/demo/views/StepPage.vue
@@ -1,22 +1,20 @@
 <template>
     <n-section theme="darkblue">
         <b-row style="min-height: 420px">
-            <b-col :cols="5">
-                <div class="introduction fade-in" :key="'1'" v-if="step === '1'">
-                    <h2>Bidrag til historien</h2>
+            <b-col :cols="4">
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vel ipsam, quisquam officiis quibusdam ipsa explicabo maiores dolorum commodi qui sunt in suscipit sint cum assumenda esse labore unde iusto?</p>
+                <h2>Din bolig - vores historie</h2>
+
+                <div class="introduction fade-in" :key="'1'" v-if="step === '1'">    
+                    <p>I anledningen af BL – Danmarks Almene Boligers 100-års jubilæum i 2019 indsamler Nationalmuseet erindringer fra beboerne i de almene boligområder i Danmark.</p>
                 </div>
 
                 <div class="introduction fade-in" :key="'2'" v-if="step === '2'">
-                    <h2>Opret en bruger</h2>
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate soluta in quod hic labore facere qui, maiores officiis ex nihil error sit nisi itaque fugiat mollitia corporis unde eum dicta.</p>
+                    <p>Vær med til at give kommende generationer mulighed for at få et indblik i, hvordan de almene boliger har skabt rammerne for mange forskellige liv, livshistorier, naboskab, formelle og uformelle fællesskaber, men også skiftende drømme og boligidealer.</p>
                 </div>
 
                 <div class="introduction fade-in" :key="'3'" v-if="step === '3'">
-                    <h2>Skriv din historie</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, rerum suscipit temporibus perspiciatis beatae esse illum pariatur atque, assumenda animi voluptas voluptatibus perferendis dignissimos? Minus voluptates a numquam fugit voluptas?</p>
+                    <p>At bevare erindringer om hverdagslivets forskelligheder er en vigtig fælles opgave. Vi er derfor interesserede i at få et så bredt udsnit af befolkningen som muligt til at deltage, så alle er velkomne til at skrive. Send dit bidrag inden d. xx.xx.2019. Du er med til at skrive fremtidens historie. </p>
                 </div>
 
                 <n-step-dot-progress 
@@ -25,8 +23,13 @@
                     :steps="steps"
                     @click="stepClick"
                 />
+
+                <div class="mt-4">
+                    <n-button variant="secondary">Læs mere</n-button>
+                    <n-button variant="primary" class="ml-4">Deltag nu</n-button>
+                </div>
             </b-col>
-            <b-col :cols="6" :offset="1">
+            <b-col :cols="7" :offset="1" style="min-height: 428px;">
                 <div class="image fade-in" :key="'1'" v-if="step === '1'">
                     <img class="full framed" src="../assets/images/summer_party_2.jpg" />
                 </div>
@@ -36,13 +39,6 @@
                 <div class="image fade-in" :key="'3'" v-if="step === '3'">
                     <img class="full framed" src="../assets/images/apartment_livingroom.jpg" />
                 </div>
-            </b-col>
-        </b-row>
-
-        <b-row>
-            <b-col>
-                <n-button variant="secondary">Læs mere</n-button>
-                <n-button variant="primary" class="ml-5">Deltag nu</n-button>
             </b-col>
         </b-row>
     </n-section>    
@@ -98,8 +94,7 @@ export default Vue.extend({
 <style lang="scss" scoped>
 
 .introduction {
-    margin-bottom: 5em;
-    min-height: 185px;
+    min-height: 170px;
 }
 
 </style>
