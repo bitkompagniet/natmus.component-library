77dc8f9bd667d2047d304fb2ff3a69d2780a3d76
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2019-02-07 10:04:09 +0100

    Cleanup deps

--- src/builds/customBootstrap.ts
+++ src/builds/customBootstrap.ts
@@ -1,7 +1,5 @@
 import { VueConstructor } from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import '../assets/scss/app.scss';
 
 const plugin = {
     install(vue: VueConstructor): void {
